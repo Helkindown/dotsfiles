d007de43af3509059043f5ecb2056ab85bcc7378
Helkindown <Helkindown@protonmail.com>
2020-08-04 07:34:32 +0200

    Changed the install script to be more easy to read

--- install.sh
+++ install.sh
@@ -11,9 +11,8 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 [ -d  "$HOME/.config/bash" ] || mkdir "$HOME/.config/bash"
 [ -d  "$HOME/bin" ] || mkdir "$HOME/bin"
 
-if which i3 2> /dev/null	# If i3 is installed, we update the ~/.config/i3 and ~/.config/i3blocks directory
+if which i3 &> /dev/null	# If i3 is installed, we update the ~/.config/i3 and ~/.config/i3blocks directory
 then
-	echo updating i3...
 
 	# If the directory doesn't exist, create it. This time for i3 specific things
 	[ -d "$HOME/bin/scripts" ] || mkdir "$HOME/bin/scripts"
@@ -23,14 +22,16 @@ then
 	# Set up the compton configuration file
 	ln -f "$BASEDIR/i3/compton.conf" "$HOME/.config"
 
-	if which xbacklight 2> /dev/null	# If the xbacklight utilities is installed,
+	echo -n "Updating i3 "
+	if which xbacklight &> /dev/null	# If the xbacklight utilities is installed,
 	then					# we presume we need the laptop version
-		echo Using laptop i3 config...
+		echo -n laptop
 		ORIGINDIR="$BASEDIR/i3/laptop"	
 	else
-		echo Using desktop i3 config...
+		echo -n desktop
 		ORIGINDIR="$BASEDIR/i3"
 	fi
+	echo " config..."
 
 	# Link all the necessary scripts to run my i3 config
 	ln -f $ORIGINDIR/bash/scripts/* $BASEDIR/i3/bash/common/* "$HOME/bin/scripts"
@@ -43,13 +44,14 @@ then
 	# Link the specific scripts needed for each configuration file
 	ln -f $ORIGINDIR/bash/src/*	"$HOME/.config/bash"
 else
-	echo i3 not detected, skipping...
+	echo i3 not detected...
 fi
 
 if which kitty &> /dev/null
 then
+	echo Updating kitty...
 	[ -d "$HOME/.config/kitty" ] || mkdir $HOME/.config/kitty
-	ln -f $ORIGINDIR/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+	ln -f $BASEDIR/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 fi
 
 # Link all the base dotfiles to either their base dir or
@@ -62,29 +64,26 @@ ln -f "$BASEDIR/bash/inputrc"		"$HOME/.inputrc"
 
 
 # Change the directory where the vimrc will be, e.i. for nvim it's ~/.config/nvim/init.vim and for Vim it's ~/.vim/vimrc
-case $EDITOR in
-	nvim)
-		VIMDIR="$HOME/.config/nvim"
-		[ -d "$VIMDIR" ] || mkdir "$VIMDIR"
+if [ -x /bin/nvim ]
+then
+	VIMDIR="$HOME/.config/nvim"
+	[ -d "$VIMDIR" ] || mkdir "$VIMDIR"
 
-		ln -f "$BASEDIR/vim/nvimrc" "$VIMDIR/init.vim"
-		;;
+	ln -f "$BASEDIR/vim/nvimrc" "$VIMDIR/init.vim"
+	echo Updating neovim...
 
-	vim)
-		VIMDIR="$HOME/.vim"
+elif [ -x /bin/vim ]
+then
+	VIMDIR="$HOME/.config/nvim"
+	[ -d "$VIMDIR" ] || mkdir "$VIMDIR"
+	echo Updating vim...
 
-		[ -d "$VIMDIR" ] || mkdir "$VIMDIR"
-		;;
-		
-	*)
-		VIMDIR=""
-		echo vim nor nvim is configured as the \$EDITOR, currenltly it is $EDITOR
-		;;
-esac
+else
+	VIMDIR=""
+	echo vim nor neovim is installed, not updating their config files
 
 if [ -n $VIMDIR ] 
 then
-	echo updating $EDITOR...
 
 	[ -d $VIMDIR/colors ] || mkdir $VIMDIR/colors
 	[ -d $VIMDIR/spell  ] || mkdir $VIMDIR/spell
