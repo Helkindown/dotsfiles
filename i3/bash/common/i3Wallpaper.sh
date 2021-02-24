e93931d89c9052d0d55d1e08dd6c9c1f15047b9f
Helkindown <Helkindown@protonmail.com>
2021-02-24 01:23:49 +0100

    Wallpaper script now have an image per screen

--- i3/bash/common/i3Wallpaper.sh
+++ i3/bash/common/i3Wallpaper.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-xwallpaper --stretch "$(find $HOME/Pictures/Wallpaper -type f | shuf -n 1)"
+IMG_DIR="$(find $HOME/Pictures/Wallpaper/* -type d | shuf -n 1 )"
+
+for SCREEN in $(xrandr | awk '/ connected/ { print $1 }')
+do
+	IMG="$(find "$IMG_DIR" -type f | shuf -n 1 )"
+	COMMAND="$COMMAND --output $SCREEN --stretch \"$IMG\""
+done
+
+echo $COMMAND | xargs xwallpaper
