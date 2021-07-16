9aa495b78b27ced705eb060ce550a42b8aafe47c
Helkindown <Helkindown@protonmail.com>
2021-07-16 23:50:46 +0200

    Added flameshot to my workflow

--- listMissingBin.sh
+++ listMissingBin.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if which which > /dev/null
+if which which &> /dev/null
 then
 
-	for elem in amixer awk bash bc cut df echo expr free git i3 i3blocks mpstat newsboat pavucontrol picom scrot sensors xclip xwallpaper
+	for EXEC in amixer awk bash bc cut df echo expr flameshot free git i3 i3blocks mpstat pavucontrol picom quiterss scrot sensors xclip xwallpaper
 	do
-		which $elem &> /dev/null || res="$res\n\t-$elem"
+		which "${EXEC}" &> /dev/null || MISSING+="\n\t- ${EXEC}"
 	done
 
 	if [ -z $res ]
 	then
-		echo "Your system seams to be able to use theses dotfiles,"
+		echo "Your system seems to be able to use theses dotfiles,"
 		echo "but still think about special font that are in use"
 	else
-		echo -e "Missing software : $res"
+		echo -e "Missing software :$res"
 	fi
 else
 	echo "which is missing and is necessary for this script, aborting..."
